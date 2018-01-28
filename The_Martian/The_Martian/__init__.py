58768ccc7d293029135262d8a28cce34cb01ea2c
feger <marc.feger@hhu.de>
2018-01-28 01:28:05 +0100

    Remove old stuff

--- The_Martian/The_Martian/__init__.py
+++
@@ -1,3 +0,0 @@
-# define all valid literals of the hex-alphabet
-hex_alphabet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                "a", "b", "c", "d", "e", "f"]
--- The_Martian/The_Martian/translation/translator.py
+++
@@ -1,8 +0,0 @@
-"""
-This file has the task to convert a text to its hex-values.
-"""
-
-# translate_to_hex should transform the text to its hex-values.
-def translate_to_hex(text):
-    hex_string = "".join([hex(ord(char)) for char in text])
-    return hex_string.replace("0x", "")
