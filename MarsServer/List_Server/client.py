45357f8c3a48f5b32d8dbaa0e93908296836141b
feger <marc.feger@hhu.de>
2018-02-15 21:33:12 +0100

    Add exeption handling

--- MarsServer/List_Server/client.py
+++ MarsServer/List_Server/client.py
@@ -3,10 +3,13 @@ import socket
 host = input('>>host: ')
 port = int(input('>>port: '))
 
-while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('[+] Waiting for input ...')
-    s.connect((host, port))
-    msg = s.recv(1024).decode('ascii')
-    s.close()
-    print('>> %s' % msg)
+try:
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('[+] Waiting for input ...')
+        s.connect((host, port))
+        msg = s.recv(1024).decode('ascii')
+        print('>> %s' % msg)
+
+except (KeyboardInterrupt, SystemExit):
+   s.close()
--- MarsServer/List_Server/server.py
+++ MarsServer/List_Server/server.py
@@ -5,9 +5,13 @@ port = int(input('>>port: '))
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((host, port))
 serversocket.listen(5)
-while True:
-    clientsocket, addr = serversocket.accept()
-    print('[+] Got a connection from %s' % str(addr))
-    msg = input('>>Text: ').encode('ascii')
-    clientsocket.send(msg)
+try:
+    while True:
+
+        clientsocket, addr = serversocket.accept()
+        print('[+] Got a connection from %s' % str(addr))
+        msg = input('>>Text: ').encode('ascii')
+        clientsocket.send(msg)
+
+except (KeyboardInterrupt, SystemExit):
     clientsocket.close()
